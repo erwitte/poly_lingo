1bc45d30a194200c99c1aa371402f00a0de5062f
Erik Witte <witte.erik2@gmail.com>
2025-01-17 12:32:02 +0100

    test environ var

--- src/app/services/linguafranca.ts
+++ src/app/services/linguafranca.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinguaFrancaService {
   private apiUrl = 'https://api.openai.com/v1/chat/completions';
-  private apiKey = '';
+  private apiKey = environment.openAIApiKey;
 
   latitude: number | null = null;
   longitude: number | null = null;
